c6ae2b8ae68a4cc474921f607eef1e96dc134549
DreBenj123 <100318209+DreBenj123@users.noreply.github.com>
2022-07-03 13:22:03 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 
-app.use("/places", require("./Controllers/places"));
-
 app.get("/", (req, res) => {
-  res.status(404).send("<h1> 404 Page</h1>");
+  res.send("Hello world!");
 });
-app.get("*", (req, res) => {});
+
+app.get("*", (req, res) => {
+  res.status(404).send("<h1>404 Page</h1>");
+});
+
 app.listen(process.env.PORT);
