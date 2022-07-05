51fc37b59ae24b2a204a2ea0f4def99ef334b184
DreBenj123 <100318209+DreBenj123@users.noreply.github.com>
2022-07-04 21:10:53 -0400

    404 page

---
+++ views/error404.jsx
@@ -0,0 +1,12 @@
+const React = require('react')
+const Def = require('./default')
+
+function error404 () {
+<Def>
+    <main>
+        <h1>404: PAGE NOT FOUND</h1>
+        <p>Oops, sorry, we cant find this page!</p>
+    </main>
+</Def>
+}
+module.expots = error404
