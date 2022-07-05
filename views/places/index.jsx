b3b4749c5b345efb85c6c8b4c2842976b495c267
DreBenj123 <100318209+DreBenj123@users.noreply.github.com>
2022-07-04 21:33:36 -0400

    places index

---
+++ views/places/index.jsx
@@ -0,0 +1,14 @@
+const React = require('react')
+const Def = require('./default')
+
+function index(){
+    return(
+    <Def>
+        <main>
+            <h1> Index </h1>
+            <p>This is the places index</p>
+        </main>
+
+    </Def>
+    )
+}
