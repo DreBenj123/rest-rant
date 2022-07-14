16e4312d8c41d72219d8c8fdf7aae453bc261834
DreBenj123 <100318209+DreBenj123@users.noreply.github.com>
2022-07-14 16:03:24 -0400

    form submission not working
    console.log rendering undefined

--- Controllers/places.js
+++ Controllers/places.js
@@ -1,11 +1,27 @@
 const router = require("express").Router();
-
+const places = require("../models/places.js");
 // GET /places/new
 router.get("/new", (req, res) => {
   res.render("places/new");
 });
 router.post("/", (req, res) => {
   console.log(req.body);
+  router.post("/", (req, res) => {
+    console.log(req.body);
+    if (!req.body.pic) {
+      // Default image if one is not provided
+      req.body.pic = "http://placekitten.com/400/400";
+    }
+    if (!req.body.city) {
+      req.body.city = "Anytown";
+    }
+    if (!req.body.state) {
+      req.body.state = "USA";
+    }
+    places.push(req.body);
+    res.redirect("/places");
+  });
+
   res.send("POST/places");
 });
 
