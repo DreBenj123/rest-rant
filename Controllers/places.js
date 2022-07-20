75b72608defe59636ed2a83c76766a417a4a6a25
DreBenj123 <100318209+DreBenj123@users.noreply.github.com>
2022-07-20 16:30:46 -0400

    form data officially going through

--- Controllers/places.js
+++ Controllers/places.js
@@ -39,20 +39,11 @@ router.get("/:id", (req, res) => {
     res.render("places/show", { place: places[id], id });
   }
 });
+//POST/places
 
 router.post("/", (req, res) => {
-  if (!req.body.pic) {
-    // Default image if one is not provided
-    req.body.pic = "http://placekitten.com/400/400";
-  }
-  if (!req.body.city) {
-    req.body.city = "Anytown";
-  }
-  if (!req.body.state) {
-    req.body.state = "USA";
-  }
-  places.push(req.body);
-  res.redirect("/places");
+  console.log(req.body);
+  res.send("POST/places");
 });
 
 module.exports = router;
